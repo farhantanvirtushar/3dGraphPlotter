d95e153add667b5a3f0a01a8ec8f7abbae7efa57
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2019-04-11 01:05:58 +0600

    slider

--- src/sample/Home.java
+++ src/sample/Home.java
@@ -7,8 +7,7 @@ import javafx.scene.SubScene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Cylinder;
-import javafx.scene.shape.Sphere;
+import javafx.scene.shape.*;
 import javafx.scene.transform.Rotate;
 
 
@@ -27,7 +26,7 @@ public class Home extends GridPane {
     Home()
     {
         graph3DSpace = new Graph3DSpace();
-        leftMenu = new LeftMenu();
+        leftMenu = new LeftMenu(graph3DSpace);
 
         Cylinder xAxis = new Cylinder(AXIS_RADIUS,AXIS_LEN);
         Cylinder yAxis = new Cylinder(AXIS_RADIUS,AXIS_LEN);
@@ -69,6 +68,71 @@ public class Home extends GridPane {
 
         this.add(menuScene,0,0);
         this.add(graphScene,1,0);
+
+
+        double totalPoints = AXIS_LEN*2;
+        float points[][] = new float[410][410];
+        for(int i=0;i<=totalPoints;i++)
+        {
+            for(int j=0;j<=totalPoints;j++)
+            {
+                float x= (float) ((i-(totalPoints/2))/2);
+                x=(float)(x/20.0);
+                float y=(float) ((j-(totalPoints/2))/2);
+                y=(float)(y/20.0);
+                float z = (float)(20*(Math.sin(x*y)));
+                points[i][j]=z;
+            }
+        }
+
+        TriangleMesh surface = new TriangleMesh();
+        surface.getTexCoords().addAll(0,0);
+
+        int k=0;
+        for(int i=0;i<totalPoints;i++)
+        {
+            for(int j=0;j<totalPoints;j++)
+            {
+                float x1= (float) ((i-(totalPoints/2))/2);
+                float y1= (float) ((j-(totalPoints/2))/2);
+                float z1 = points[i][j];
+
+                float x2= (float) (x1+0.5);
+                float y2= y1;
+                float z2 = points[i+1][j];
+
+                float x3= x1;
+                float y3= (float) (y1+0.5);
+                float z3 = points[i][j+1];
+
+                float x4= (float) (x1+0.5);
+                float y4= (float) (y1+0.5);
+                float z4 = points[i+1][j+1];
+
+
+                surface.getPoints().addAll(
+                        x1,    y1,    z1,
+                        x2,    y2,    z2,
+                        x3,    y3,    z3,
+                        x4,    y4,    z4
+                );
+
+                surface.getFaces().addAll(
+                        k+0,0,  k+1,0,  k+2,0,
+                        k+2,0,  k+1,0,  k+0,0,
+                        k+1,0,  k+2,0,  k+3,0,
+                        k+3,0,  k+2,0,  k+1,0
+
+                );
+                k+=4;
+
+            }
+
+        }
+        MeshView graphSurface = new MeshView(surface);
+        graphSurface.setDrawMode(DrawMode.FILL);
+        graphSurface.setMaterial(new PhongMaterial(Color.GRAY));
+        graph3DSpace.getChildren().addAll(graphSurface);
 
 
     }
--- src/sample/LeftMenu.java
+++ src/sample/LeftMenu.java
@@ -1,27 +1,107 @@
 package sample;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
 
 public class LeftMenu extends Group {
+    Graph3DSpace graph3DSpace;
     Button function[] = new Button[5];
     VBox vBox;
 
-    LeftMenu()
+    Slider xSlider;
+    Slider ySlider;
+    Slider zSlider;
+
+    Label rotateBy;
+    Label xLabel;
+    Label yLabel;
+    Label zLabel;
+
+    LeftMenu(Graph3DSpace graph3DSpace)
     {
+        this.graph3DSpace=graph3DSpace;
         vBox = new VBox();
         for(int i=0;i<function.length;i++)
         {
             function[i] = new Button("Add New Graph");
-            function[i].setPrefSize(200,100);
+            function[i].setPrefSize(200,90);
             String id = "f"+i;
             function[i].setId(id);
             function[i].setVisible(true);
             vBox.getChildren().add(function[i]);
         }
         function[0].setVisible(true);
+
+        rotateBy = new Label("\nRotate By Axis\n");
+        rotateBy.setScaleX(1);
+        rotateBy.setScaleY(1);
+        rotateBy.setTextFill(Color.BROWN);
+        vBox.getChildren().add(rotateBy);
+
+        xSlider = new Slider();
+        ySlider = new Slider();
+        zSlider = new Slider();
+        xLabel = new Label("X Axis");
+        yLabel = new Label("Y Axis");
+        zLabel = new Label("Z Axis");
+        xLabel.setTextFill(Color.RED);
+        yLabel.setTextFill(Color.GREEN);
+        zLabel.setTextFill(Color.BLUE);
+
+        xSlider.setMax(360);
+        xSlider.setValue(0);
+        xSlider.setShowTickLabels(true);
+        xSlider.setShowTickMarks(true);
+        xSlider.setMajorTickUnit(90);
+        xSlider.setMinorTickCount(2);
+        xSlider.setBlockIncrement(10);
+
+        ySlider.setMax(360);
+        ySlider.setValue(0);
+        ySlider.setShowTickLabels(true);
+        ySlider.setShowTickMarks(true);
+        ySlider.setMajorTickUnit(90);
+        ySlider.setMinorTickCount(2);
+        ySlider.setBlockIncrement(10);
+
+        zSlider.setMax(360);
+        zSlider.setValue(0);
+        zSlider.setShowTickLabels(true);
+        zSlider.setShowTickMarks(true);
+        zSlider.setMajorTickUnit(90);
+        zSlider.setMinorTickCount(2);
+        zSlider.setBlockIncrement(10);
+
+        xSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldNumber, Number newNumber) {
+                int delta = oldNumber.intValue() - newNumber.intValue();
+                graph3DSpace.rotateByX(delta);
+            }
+        });
+        ySlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldNumber, Number newNumber) {
+                int delta = oldNumber.intValue() - newNumber.intValue();
+                graph3DSpace.rotateByY(delta);
+            }
+        });
+        zSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldNumber, Number newNumber) {
+                int delta = oldNumber.intValue() - newNumber.intValue();
+                graph3DSpace.rotateByZ(delta);
+            }
+        });
+        vBox.getChildren().addAll(xLabel,xSlider,yLabel,ySlider,zLabel,zSlider);
         this.getChildren().add(vBox);
     }
 
